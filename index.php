1dbbf8ce8df6b356b9c085ac44c5a7cd2212d016
dtutm <utumiandrial@gmail.com>
2019-11-19 17:20:58 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>SimpleBBS by DTUTM</title>
+        <link rel="stylesheet" href="forIndex.css">
+    </head>
+
+
+    <body>
+    <div class="container">
+    <span class="main-form">
+    <h1 class="title-text">SimpleBBS</h1>
+
+    <form method="post">
+        <p>
+            <span class="form-name">Login ID</span>
+            <input class="form-design" type="text" name="userId">
+        </p>
+        <p>
+            <span class="form-name">Password</span>
+            <input class="form-design" type="password" name="userPassword">
+        </p>
+        <input type="submit" value="ログイン">
+    </form>    
+    </span>
+    </div>
+    
+    </body>
+
+</html>
